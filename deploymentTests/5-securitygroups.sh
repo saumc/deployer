0d9240a31c2ef6f622916fda015a129c21ae9e93
SC <4994826+saumc@users.noreply.github.com>
2021-03-20 15:10:31 +0530

    secutiry gp test - debug 8

--- deploymentTests/5-securitygroups.sh
+++ deploymentTests/5-securitygroups.sh
@@ -4,56 +4,10 @@ which go
 sudo go get -u github.com/jvehent/pineapple
 echo which pipeapple 
 which pineapple
-echo Running security groups check
+echo Running security groups check 
+echo pineapple version - gopath 
+$GOPATH/bin/pineapple -V
 
-$GOPATH/bin/pineapple <<EOF
-aws:
-    region: ap-south-1
-    accountnumber: 690890622406
+echo pineapple version - straight 
+pineapple -V
 
-components:
-    - name: load-balancer
-      type: elb
-      tag:
-          key: elasticbeanstalk:environment-name
-          value: invoicer-api
-
-    - name: application
-      type: ec2
-      tag: 
-          key: elasticbeanstalk:environment-name
-          value: invoicer-api
-
-    - name: database
-      type: rds
-      tag:
-          key: environment-name
-          value: invoicer-api
-
-#    - name: bastion
-#      type: ec2
-#      tag:
-#          key: environment-name
-#          value: invoicer-bastion
-
-rules:
-    - src: 0.0.0.0/0
-      dst: load-balancer
-      dport: 443
-
-    - src: load-balancer
-      dst: application
-      dport: 80
-
-    - src: application
-      dst: database
-      dport: 5432
-
-#    - src: bastion
-#      dst: application
-#      dport: 22
-#
-#    - src: bastion
-#      dst: database
-#      dport: 5432
-EOF
