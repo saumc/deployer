879e6addefd2a8cb64b129af6ab65770ec8dfa44
SC <4994826+saumc@users.noreply.github.com>
2021-03-20 13:48:15 +0530

    secutiry gp test - debug 4

--- deploymentTests/5-securitygroups.sh
+++ deploymentTests/5-securitygroups.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo Install pipeapple first
 which go
-#go get -u github.com/jvehent/pineapple
+go get github.com/jvehent/pineapple
 which pineapple
 echo Running security groups check
