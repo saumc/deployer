377f6a75e90fd3423936f4a6adb4b220015c26a2
SC <4994826+saumc@users.noreply.github.com>
2021-03-20 21:53:40 +0530

    secutiry gp test - debug 10

--- deploymentTests/5-securitygroups.sh
+++ deploymentTests/5-securitygroups.sh
@@ -2,63 +2,20 @@
 echo Install pipeapple first
 which go
 sudo go get -u github.com/jvehent/pineapple
-echo which pipeapple 
+#echo which pipeapple 
 which pineapple
 echo Running security groups check 
-echo pineapple version - gopath 
+#echo pineapple version - gopath 
 $GOPATH/bin/pineapple -V
 
 echo pineapple version - straight 
 pineapple -V
 
-$GOPATH/bin/pineapple <<EOF
-aws:
-    region: ap-south-1
-    accountnumber: 690890622406
+rundir=`pwd`
 
-components:
-    - name: load-balancer
-      type: elb
-      tag:
-          key: elasticbeanstalk:environment-name
-          value: invoicer-api
 
-    - name: application
-      type: ec2
-      tag: 
-          key: elasticbeanstalk:environment-name
-          value: invoicer-api
+echo "Locate yaml?"
+ls $rundir/deploymentTests/myconfig.yaml
 
-    - name: database
-      type: rds
-      tag:
-          key: environment-name
-          value: invoicer-api
 
-#    - name: bastion
-#      type: ec2
-#      tag:
-#          key: environment-name
-#          value: invoicer-bastion
-
-rules:
-    - src: 0.0.0.0/0
-      dst: load-balancer
-      dport: 443
-
-    - src: load-balancer
-      dst: application
-      dport: 80
-
-    - src: application
-      dst: database
-      dport: 5432
-
-#    - src: bastion
-#      dst: application
-#      dport: 22
-#
-#    - src: bastion
-#      dst: database
-#      dport: 5432
-EOF
+$GOPATH/bin/pineapple -c $rundir/deploymentTests/myconfig.yaml
